b384b20e73142e541201bd5c3b62222f78faec22
iurie-bogdanovici <iubogdanovici@gmail.com>
2022-06-28 21:03:15 +0300

    Sample React App JS

---
+++ src/components/Description.js
@@ -0,0 +1,11 @@
+// Description component
+const Description = () => {
+  return (
+    <div className="description">
+      This is a <span className="italic">Test Application</span> using React
+      JS. <br /> <br />
+    </div>
+  )
+}
+
+export default Description
